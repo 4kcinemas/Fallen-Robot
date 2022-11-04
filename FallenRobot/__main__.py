24bef6192ba5583c76a053ff2eec7592e254f1f6
4kcinemas <98001401+4kcinemas@users.noreply.github.com>
2022-11-04 21:48:43 +0530

    Update __main__.py

--- FallenRobot/__main__.py
+++ FallenRobot/__main__.py
@@ -208,9 +208,7 @@ def start(update: Update, context: CallbackContext):
             first_name = update.effective_user.first_name
             update.effective_message.reply_photo(
                 START_IMG
-            )
-            update.effective_message.reply_text(
-                PM_START_TEXT.format(escape_markdown(first_name), BOT_NAME, uptime),
+                caption=PM_START_TEXT.format(escape_markdown(first_name), BOT_NAME, uptime),
                 reply_markup=InlineKeyboardMarkup(buttons),
                 parse_mode=ParseMode.MARKDOWN,
                 timeout=60,
